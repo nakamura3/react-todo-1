ba3ef46ee7d1df6eae64bb1956f7ebee246a2429
nakamura3 <nakamu@ines.co.jp>
2023-02-23 09:23:40 +0900

    入力機能を実装

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 // react-todo-1: main component
+import { useState } from 'react';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    { id: 0, title: 'デフォルトTODO' }
+  ]);
+  const [uniqueId, setUniqueId] = useState(1);
 
-  // データ
-  const tasks = [
-    { title: 'Todo1つ目', id: 0 },
-    { title: 'Todo2つ目', id: 1 }
-  ];
+  const addTodo = (title) => {
+    setTasks(tasks => [...tasks, {id: uniqueId, title: title}]);
+    setUniqueId(id => id + 1);
+  }
 
   return (
     <div>
       <h1>TODO App</h1>
-      <TodoInput />
+      <TodoInput addTodo={addTodo} />
       <TodoList tasks={tasks} />
     </div>
   );
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,10 +1,21 @@
 // react-todo-1: input component
+import { useState } from 'react';
 
-const TodoInput = () => {
+const TodoInput = ({ addTodo }) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = e => setInputValue(e.target.value);
+    const handleClick = () => {
+        addTodo(inputValue);
+        setInputValue('');
+    };
+
     return (
         <div>
-            <input placeholder="新規TODOを入力してください" />
-            <button>登録</button>
+            <input placeholder="新規TODOを入力してください"
+                value={inputValue}
+                onChange={handleChange} />
+            <button onClick={handleClick} >登録</button>
         </div>
     )
 };
